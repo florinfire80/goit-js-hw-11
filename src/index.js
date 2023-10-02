f00bdc491483bdac6e9cf9d8ee04ccc93837af5e
Florin <florinfire80@yahoo.com>
2023-10-02 08:36:04 +0300

    update task

--- src/index.js
+++ src/index.js
@@ -1,79 +1,78 @@
+import Notiflix from 'notiflix';
 import { searchImages } from './pixabay-api.js';
 
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
-const loadMoreButton = document.querySelector('.load-more');
 
 let currentPage = 1;
 let currentQuery = '';
 
-// Funcția pentru afișarea imaginilor
-function displayImages(images) {
-  images.forEach(image => {
-    const photoCard = document.createElement('div');
-    photoCard.classList.add('photo-card');
-
-    const img = document.createElement('img');
-    img.loading = 'lazy';
-    img.src = image.webformatURL;
-    img.alt = image.tags;
-
-    const info = document.createElement('div');
-    info.classList.add('info');
-
-    const likes = document.createElement('p');
-    likes.classList.add('info-item');
-    likes.innerHTML = `<b>Likes:</b> ${image.likes}`;
-
-    const views = document.createElement('p');
-    views.classList.add('info-item');
-    views.innerHTML = `<b>Views:</b> ${image.views}`;
-
-    const comments = document.createElement('p');
-    comments.classList.add('info-item');
-    comments.innerHTML = `<b>Comments:</b> ${image.comments}`;
-
-    const downloads = document.createElement('p');
-    downloads.classList.add('info-item');
-    downloads.innerHTML = `<b>Downloads:</b> ${image.downloads}`;
-
-    info.appendChild(likes);
-    info.appendChild(views);
-    info.appendChild(comments);
-    info.appendChild(downloads);
-
-    photoCard.appendChild(img);
-    photoCard.appendChild(info);
-
-    gallery.appendChild(photoCard);
-  });
-}
+const observer = new IntersectionObserver(handleLoadMore, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5,
+});
 
 // Funcția pentru gestionarea căutării
-function handleSearch(event) {
+async function handleSearch(event) {
   event.preventDefault();
+
+  // Obțineți valoarea din câmpul de input
+  const searchQuery = form.searchQuery.value.trim();
+
+  // Verificați dacă câmpul de input este gol
+  if (!searchQuery) {
+    // Afișați un mesaj de eroare sau faceți altă acțiune corespunzătoare
+    console.error('Please enter a search query.');
+    return; // Nu faceți solicitarea către API dacă câmpul este gol
+  }
+
   gallery.innerHTML = ''; // Șterge imaginile existente din galerie
   currentQuery = form.searchQuery.value.trim(); // Obține valoarea căutării
   currentPage = 1; // Resetează pagina la 1 pentru o nouă căutare
 
-  // Ascunde butonul "Load more" după o nouă căutare
-  loadMoreButton.style.display = 'none';
+  try {
+    const { images, totalHits } = await searchImages(
+      currentQuery,
+      currentPage,
+      gallery
+    );
 
-  // Apelează funcția searchImages pentru a efectua căutarea
-  searchImages(currentQuery, currentPage, gallery);
+    if (images.length > 0) {
+      // Afisati mesajul doar daca exista cel putin o imagine
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
+
+    if (images.length > 0 && totalHits > images.length) {
+      observer.observe(document.querySelector('.photo-card:last-child'));
+    }
+  } catch (error) {
+    console.error('Error searching for images:', error);
+  }
 }
 
 // Adaugă eveniment pentru formularul de căutare
 form.addEventListener('submit', handleSearch);
 
 // Funcția pentru gestionarea încărcării suplimentare
-function handleLoadMore() {
-  currentPage += 1; // Incrementăm pagina pentru încărcare suplimentară
-  searchImages(currentQuery, currentPage);
+async function handleLoadMore(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      currentPage += 1; // Incrementăm pagina pentru încărcare suplimentară
 
-  // Afișează butonul "Load more" după solicitarea de încărcare suplimentară
-  loadMoreButton.style.display = 'block';
+      searchImages(currentQuery, currentPage, gallery)
+        .then(({ images, totalHits }) => {
+          if (images.length > 0) {
+            displayImages(images);
+          }
+
+          if (totalHits <= currentPage * 40) {
+            observer.unobserve(entry.target);
+          }
+        })
+        .catch(error => {
+          console.error('Error loading more images:', error);
+        });
+    }
+  });
 }
-
-// Adaugă eveniment pentru butonul "Load more"
-loadMoreButton.addEventListener('click', handleLoadMore);
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const apiKey = '39735160-014c6c17620a2b57de6626257';
 
 // Funcția pentru căutare imagini
 
 export async function searchImages(query, page = 1, gallery) {
-  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}`;
+  const perPage = 40; // Numărul de imagini pe pagină
+  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
   try {
     const response = await axios.get(apiUrl);
@@ -23,7 +26,7 @@ export async function searchImages(query, page = 1, gallery) {
       displayImages(images, gallery);
     }
 
-    return { images, totalHits };
+    return { images, totalHits, currentPage: page };
   } catch (error) {
     console.error('Error fetching data from Pixabay API:', error);
     throw error; // Aruncă eroarea pentru a fi gestionată ulterior în cod
@@ -36,38 +39,82 @@ function displayImages(images, gallery) {
     const photoCard = document.createElement('div');
     photoCard.classList.add('photo-card');
 
+    const imgLink = document.createElement('a'); // Creează un link pentru imagine
+    imgLink.href = image.largeImageURL; // Setează URL-ul imaginii mari
+
     const img = document.createElement('img');
     img.loading = 'lazy';
     img.src = image.webformatURL;
     img.alt = image.tags;
 
+    imgLink.appendChild(img); // Adaugă imaginea în link
+
     const info = document.createElement('div');
     info.classList.add('info');
 
+    const propertiesContainer = document.createElement('div');
+    propertiesContainer.classList.add('info-properties');
+
+    const valuesContainer = document.createElement('div');
+    valuesContainer.classList.add('info-values');
+
     const likes = document.createElement('p');
     likes.classList.add('info-item');
-    likes.innerHTML = `<b>Likes:</b> ${image.likes}`;
+    likes.innerHTML = `<b>Likes</b>`;
 
     const views = document.createElement('p');
     views.classList.add('info-item');
-    views.innerHTML = `<b>Views:</b> ${image.views}`;
+    views.innerHTML = `<b>Views</b>`;
 
     const comments = document.createElement('p');
     comments.classList.add('info-item');
-    comments.innerHTML = `<b>Comments:</b> ${image.comments}`;
+    comments.innerHTML = `<b>Comments</b>`;
 
     const downloads = document.createElement('p');
     downloads.classList.add('info-item');
-    downloads.innerHTML = `<b>Downloads:</b> ${image.downloads}`;
+    downloads.innerHTML = `<b>Downloads</b>`;
 
-    info.appendChild(likes);
-    info.appendChild(views);
-    info.appendChild(comments);
-    info.appendChild(downloads);
+    const likesValue = document.createElement('p');
+    likesValue.classList.add('info-value');
+    likesValue.innerText = image.likes;
 
-    photoCard.appendChild(img);
+    const viewsValue = document.createElement('p');
+    viewsValue.classList.add('info-value');
+    viewsValue.innerText = image.views;
+
+    const commentsValue = document.createElement('p');
+    commentsValue.classList.add('info-value');
+    commentsValue.innerText = image.comments;
+
+    const downloadsValue = document.createElement('p');
+    downloadsValue.classList.add('info-value');
+    downloadsValue.innerText = image.downloads;
+
+    propertiesContainer.appendChild(likes);
+    propertiesContainer.appendChild(views);
+    propertiesContainer.appendChild(comments);
+    propertiesContainer.appendChild(downloads);
+
+    valuesContainer.appendChild(likesValue);
+    valuesContainer.appendChild(viewsValue);
+    valuesContainer.appendChild(commentsValue);
+    valuesContainer.appendChild(downloadsValue);
+
+    info.appendChild(propertiesContainer);
+    info.appendChild(valuesContainer);
+
+    photoCard.appendChild(imgLink); // Adaugă linkul cu imaginea
     photoCard.appendChild(info);
 
     gallery.appendChild(photoCard);
   });
+
+  // Inițializați SimpleLightbox pentru galeria dvs.
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+  // Refresh la Lightbox pentru a include noile imagini
+  lightbox.refresh();
 }
